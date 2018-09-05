7db89c172b010096bec121f5ca354549a94038fa
Jeremy Wood <farachan@gmail.com>
2018-09-05 10:33:12 -0400

    Added material-ui.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import createBrowserHistory from 'history/createBrowserHistory';
+import { Redirect, Route, Router } from 'react-router';
+import { getMuiTheme, MuiThemeProvider } from 'material-ui';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Redirect, Route, Router } from 'react-router';
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    color: '#37517E',
+    height: 50,
+  },
+});
 
 const customHistory = createBrowserHistory();
 
 const Root = () => (
-  <Router history={customHistory}>
-    <div>
-      <Route path="/login" component={App} />
-      <Redirect from="/" to="/login" />
-    </div>
-  </Router>
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router history={customHistory}>
+      <div>
+        <Route path="/login" component={App} />
+        <Redirect from="/" to="/login" />
+      </div>
+    </Router>
+  </MuiThemeProvider>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
