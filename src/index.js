e340b66bb476eacf8192bb736db5e33eba83220a
Jeremy Wood <farachan@gmail.com>
2018-09-05 10:29:41 -0400

    Added react-router.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import createBrowserHistory from 'history/createBrowserHistory';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Redirect, Route, Router } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customHistory = createBrowserHistory();
+
+const Root = () => (
+  <Router history={customHistory}>
+    <div>
+      <Route path="/login" component={App} />
+      <Redirect from="/" to="/login" />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
 registerServiceWorker();
