00f339343c92480b34a96651dff368b99ea24b0d
Jeremy Wood <farachan@gmail.com>
2018-10-09 09:41:21 -0400

    Removed example code from App.js.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { db } from './config/constants';
 import SpecimenList from './features/SpecimenList';
 
 class App extends Component {
   state = {
     name: 'Welcome to React',
     suggestions: [],
-  };
-
-  componentDidMount() {
-    db.doc('courses/online')
-      .get()
-      .then(doc => this.setState({ name: doc.data().name }));
-    db.collection('suggestions').onSnapshot(collection => {
-      const suggestions = collection.docs.map(doc => doc.data());
-      this.setState({ suggestions });
-    });
-  }
-
-  handleProjectTitle = e => {
-    e.preventDefault();
-    db.doc('courses/online').set({ name: this.titleName.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    // db.collection('suggestions').add({ name: this.suggestionInput.value });
-    //step 1: create the reference
-    const newSuggestionReference = db.collection('suggestions').doc();
-    //step 2: update the reference with the data
-    newSuggestionReference.set({
-      name: this.suggestionInput.value,
-      id: newSuggestionReference.id,
-    });
   };
 
   render() {
