a2bdcd06319ac412519ed3b41c3d6d533cdb9bfe
Jeremy Wood <farachan@gmail.com>
2018-10-09 02:27:24 -0400

    Added Placeholder component for showing detail sections in layout.

--- src/features/SpecimenDetail.js
+++ src/features/SpecimenDetail.js
@@ -5,37 +5,31 @@ import Locality from './detail/Locality';
 import DetailGrid from './detail/layout/DetailGrid';
 import DetailColumn from './detail/layout/DetailColumn';
 import DetailItem from './detail/layout/DetailItem';
+import Placeholder from './detail/Placeholder';
 
 const SpecimenDetail = props => {
-  const createDetails = data => {
-    console.log(data.locality);
-    return (
-      <DetailGrid>
-        <DetailColumn>
-          <DetailItem title="Identification">
-            <Identification data={data} />
+  const createDetails = data => (
+    <DetailGrid>
+      <DetailColumn>
+        <DetailItem title="Identification">
+          <Identification data={data} />
+        </DetailItem>
+        {data.locality && (
+          <DetailItem title="Locality and Collection Details">
+            <Locality data={data} />
           </DetailItem>
-          {data.locality && (
-            <DetailItem title="Locality and Collection Details">
-              <Locality data={data} />
-            </DetailItem>
-          )}
-        </DetailColumn>
-        <DetailColumn>
-          {data.locality && (
-            <DetailItem title="Locality and Collection Details">
-              <Locality data={data} />
-            </DetailItem>
-          )}
-          {data.locality && (
-            <DetailItem>
-              <Locality data={data} />
-            </DetailItem>
-          )}
-        </DetailColumn>
-      </DetailGrid>
-    );
-  };
+        )}
+      </DetailColumn>
+      <DetailColumn>
+        <DetailItem title="Extra Details">
+          <Placeholder data={data} />
+        </DetailItem>
+        <DetailItem>
+          <Placeholder data={data} />
+        </DetailItem>
+      </DetailColumn>
+    </DetailGrid>
+  );
 
   if (props.specimen) {
     return createDetails(props.specimen);
---
+++ src/features/detail/Placeholder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
+const Placeholder = ({ classes, data: { locality } }) => (
+  <Card>
+    <Typography>Placeholder</Typography>
+  </Card>
+);
+
+export default Placeholder;
