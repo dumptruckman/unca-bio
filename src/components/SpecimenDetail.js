b1b4333171cbe39c3bcd17120acf62c2d46fc0c3
Jeremy Wood <farachan@gmail.com>
2018-11-16 23:08:23 -0500

    Refactored SpecimenDetail with more destructuring.

--- src/components/SpecimenDetail.js
+++ src/components/SpecimenDetail.js
@@ -10,43 +10,48 @@ import Collectors from './detail/Collectors';
 import Attributes from './detail/Attributes';
 
 const SpecimenDetail = props => {
-  const createDetails = data => (
-    <DetailGrid>
-      <DetailColumn>
-        {(data.identification || props.editing) && (
-          <DetailItem title="Identification">
-            <Identification data={data} editing={props.editing} />
-          </DetailItem>
-        )}
-        {(data.locality || props.editing) && (
-          <DetailItem title="Locality and Collection Details">
-            <Locality data={data} editing={props.editing} />
-          </DetailItem>
-        )}
-        {data.collectors && (
-          <DetailItem title="Collector(s)">
-            <Collectors data={data} editing={props.editing} />
-          </DetailItem>
-        )}
-      </DetailColumn>
-      <DetailColumn>
-        <DetailItem title="Extra Details">
-          <Placeholder data={data} editing={props.editing} />
-        </DetailItem>
-        {(data.attributes || props.editing) && (
-          <DetailItem title="Attributes">
-            <Attributes data={data} editing={props.editing} />
-          </DetailItem>
-        )}
-        <DetailItem>
-          <Placeholder data={data} editing={props.editing} />
-        </DetailItem>
-      </DetailColumn>
-    </DetailGrid>
-  );
+  const { editing, specimen } = props;
 
-  if (props.specimen) {
-    return createDetails(props.specimen);
+  const createDetails = data => {
+    const { identification, locality, collectors, attributes } = data;
+    return (
+      <DetailGrid>
+        <DetailColumn>
+          {(identification || editing) && (
+            <DetailItem title="Identification">
+              <Identification data={data} editing={editing} />
+            </DetailItem>
+          )}
+          {(locality || editing) && (
+            <DetailItem title="Locality and Collection Details">
+              <Locality data={data} editing={editing} />
+            </DetailItem>
+          )}
+          {collectors && (
+            <DetailItem title="Collector(s)">
+              <Collectors data={data} editing={editing} />
+            </DetailItem>
+          )}
+        </DetailColumn>
+        <DetailColumn>
+          <DetailItem title="Extra Details">
+            <Placeholder data={data} editing={editing} />
+          </DetailItem>
+          {(attributes || editing) && (
+            <DetailItem title="Attributes">
+              <Attributes data={data} editing={editing} />
+            </DetailItem>
+          )}
+          <DetailItem>
+            <Placeholder data={data} editing={editing} />
+          </DetailItem>
+        </DetailColumn>
+      </DetailGrid>
+    );
+  };
+
+  if (specimen) {
+    return createDetails(specimen);
   } else {
     return (
       <FirestoreDocument
